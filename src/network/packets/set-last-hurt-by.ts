5ca35dafe60c1932fd411f27084fc0b8d220c2a8
bonanoo <ainaraspirmas@gmail.com>
2025-02-20 17:53:40 +0200

    temp packet

---
+++ src/network/packets/set-last-hurt-by.ts
@@ -0,0 +1,8 @@
+import { DataPacket } from "@serenityjs/protocol";
+import { Create, Serialize } from "@sanctumterra/raknet";
+import { VarInt } from "@serenityjs/binarystream";
+
+@Create(96)
+export class SetLastHurtByPacket extends DataPacket {
+    @Serialize(VarInt) public lastHurtBy!: bigint;
+}
