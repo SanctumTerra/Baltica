a9ec014b2677dc81ccd3e75678653e4bed07c098
bonanoo <ainaraspirmas@gmail.com>
2025-02-20 17:53:24 +0200

    temp packet

---
+++ src/network/packets/update-player-game-type.ts
@@ -0,0 +1,10 @@
+import { DataPacket } from "@serenityjs/protocol";
+import { Create, Serialize } from "@sanctumterra/raknet";
+import { ZigZag, ZigZong, VarLong } from "@serenityjs/binarystream";
+
+@Create(151)
+export class UpdatePlayerGameTypePacket extends DataPacket {
+    @Serialize(ZigZag) public gameType!: number;
+    @Serialize(ZigZong) public uniqueId!: bigint;
+    @Serialize(VarLong) public playerGamemode!: bigint;
+}
