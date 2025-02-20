0686f03d1a1e0a69d7cb46bee8bd4757c0a592fa
bonanoo <ainaraspirmas@gmail.com>
2025-02-20 17:53:48 +0200

    temp packet

---
+++ src/network/packets/set-default-gamemode.ts
@@ -0,0 +1,8 @@
+import { DataPacket } from "@serenityjs/protocol";
+import { Create, Serialize } from "@sanctumterra/raknet";
+import { ZigZag } from "@serenityjs/binarystream";
+
+@Create(105)
+export class SetDefaultGamemodePacket extends DataPacket {
+    @Serialize(ZigZag) public gamemode!: number;
+} 
