3f63ce93c84a1540f6e84650fb113ea5a0a28b18
Bonanoo <61703768+AnyBananaGAME@users.noreply.github.com>
2025-07-05 14:30:03 +0300

    Create player-events.ts
    Player Events

---
+++ src/server/types/player-events.ts
@@ -0,0 +1,10 @@
+import { PacketNames } from "src/client";
+import type * as Protocol from "@serenityjs/protocol";
+
+export type PlayerEvents = {
+	[K in PacketNames]: [packet: InstanceType<(typeof Protocol)[K]>];
+} & {
+	packet: [Protocol.DataPacket];
+	error: [Error];
+	login: [];
+};
