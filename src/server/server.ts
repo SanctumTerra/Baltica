d7b4ec273c188bb2d954b917cf0133339dbd0e73
Bonanoo <61703768+AnyBananaGAME@users.noreply.github.com>
2025-07-05 14:30:12 +0300

    Create server.ts
    Server!!

---
+++ src/server/server.ts
@@ -0,0 +1,61 @@
+import { Connection, Logger, Server as RaknetServer } from "@sanctumterra/raknet";
+import { Emitter } from "../libs";
+import { defaultServerOptions, ServerEvents, ServerOptions } from "./types";
+import { Player } from "./player";
+import { DisconnectMessage, DisconnectPacket, DisconnectReason } from "@serenityjs/protocol";
+
+export class Server extends Emitter<ServerEvents> {
+    options: ServerOptions;
+    raknet: RaknetServer;
+    players: Map<string, Player> = new Map();
+
+    constructor(options: Partial<ServerOptions>){
+        super();
+        this.options = { ...defaultServerOptions, ...options };
+        this.raknet = new RaknetServer({
+            host: this.options.address,
+            port: this.options.port,
+            motd: this.options.motd,
+            tickRate: this.options.tickRate
+        });
+    }
+
+    public getConKey(connection: Connection) {
+        const addr = connection.getAddress();
+		return `${addr.address}:${addr.port}`;
+    }
+
+    public start() {
+        this.raknet.on("connect", (connection) => {
+            const key = this.getConKey(connection);
+            const existingPlayer = this.players.get(key);
+
+            if(existingPlayer) {
+                try {
+                    const disconnectPacket = new DisconnectPacket();
+	    			disconnectPacket.message = new DisconnectMessage("New connection has been created with the same Address..", );
+		    		disconnectPacket.reason = DisconnectReason.Kicked;
+			    	disconnectPacket.hideDisconnectScreen = false;
+                    existingPlayer.send(disconnectPacket);
+                } catch (error) {
+                    Logger.error((error as Error))
+                }
+            }
+
+            const player = new Player(this, connection);
+            this.players.set(key, player);
+            this.emit("playerConnect", player);
+            const { address, port } = connection.getAddress();
+            Logger.info(`Session received from: ${address}:${port}`);
+        });
+        this.raknet.start();
+    }
+
+    public onDisconnect(player: Player) {
+        const key = this.getConKey(player.connection);
+		const displayName = player.username ?? key; // Just cause 
+		Logger.info("Player disconnected: ", displayName);
+		this.emit("disconnect", displayName, player);
+		this.players.delete(key);
+	}
+}
